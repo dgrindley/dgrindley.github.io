8bd7e7d4d854ebcaa383b69f6e4d4611977c2a2f
dgrindley <devlingrindley@gmail.com>
2018-05-13 17:33:15 -0500

    Basic functionality complete
    viewing results is now possible, still need to add a few things, but site is for the most part complete

--- CSC175/js/define.js
+++ CSC175/js/define.js
@@ -1,3 +1,48 @@
+// global array for data to use in table
+var currentResults = [];
+
+$(document).ready(function () {
+    // click event handlers for dynamic elements (https://stackoverflow.com/a/9331127)
+    $("#keywordLinks").on("click", "a.list-group-item", function() {
+        // console.log($(this).text());
+
+        for (var i = 0; i < currentResults.length; i++) {
+            var r = currentResults[i]; // easier to type
+
+            if (r.keyword == $(this).text()) {
+                console.log(r.keyword);
+
+                // populate table with results here!
+                // r.data[0].japanese[0].word
+                // r.data[0].japanese[1].reading
+                // r.data[0].senses[0].english_definitions[#]
+
+                $("#results").find("tr:gt(0)").remove(); // clear rows w/o clearing headings
+
+                // e = entry
+                for (var e of r.data) {
+                    // this'll cut out alternate spellings/readings, so be careful
+                    var word = e.japanese[0].word !== undefined ? e.japanese[0].word : "";
+                    var reading = e.japanese[0].reading !== undefined ? e.japanese[0].reading : "";
+
+                    // build english definition
+                    var english = "";
+                    
+                    for (var d of e.senses[0].english_definitions) {
+                        english += (d + ", ")
+                    }
+
+                    english = english.slice(0, -2); // cut off trailing space + comma
+
+                    console.log(word + " | " + reading + " | " + english);
+
+                    $("#results").append("<tr><td>" + word + "</td><td>" + reading + "</td><td>" + english + "</td></tr>");
+                }
+            }
+        }
+    });
+});
+
 var getDictEntries = function() {
     var words = [];
     var sentence = $("#sentence").val();
@@ -38,23 +83,37 @@ var getDictEntries = function() {
         }
 
         console.log(words);
-        console.log(searchJisho(words));
+        // console.log(searchJisho(words));
+        currentResults = searchJisho(words);
+        console.log(currentResults);
+
+        // clear then set up links to words in list group
+        $("#keywordLinks").empty();
+        for (var w of words) {
+            $("#keywordLinks").append('<a href="#" class="list-group-item">' + w + '</a>');
+        }
     }
 }
 
+// searches Jisho and returns an array containing results
 var searchJisho = function(query) {
-    // var jishoAPI = "https://jisho.org/api/v1/search/words?keyword="
-    var jishoAPI = "https://jisho.org/api/v1/search/words?"
+    // cors.io is a proxy service for bypassing the "No 'Access-Control-Allow-Origin' header" error
+    // This is required to avoid making the user change their browser settings and also because GitHub pages
+    // seems to not support CORS (although https://stackoverflow.com/a/26417091 says otherwise?)
+    var jishoAPI = "https://cors.io/?https://jisho.org/api/v1/search/words?"
     var results = [];
 
     for (var i = 0; i < query.length; i++) {
-        $.getJSON(jishoAPI, {
-            keyword: query[i]
-        })
-        .done(function(data) {
-            console.log(data);
-            results[i] = data;
-        });
+        (function(index) {
+            $.getJSON(jishoAPI, {
+                keyword: query[i]
+            })
+            .done(function(data) {
+                console.log(data);
+                data.keyword = query[index];
+                results.push(data);
+            });
+        })(i);
     }
 
     return results;

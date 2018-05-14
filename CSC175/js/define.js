03dc8ee0805054ab6ec2410269c5299c86471f3d
dgrindley <devlingrindley@gmail.com>
2018-05-14 14:06:23 -0500

    Detailed view modal + feedback form
    Maybe done?

--- CSC175/js/define.js
+++ CSC175/js/define.js
@@ -1,47 +1,175 @@
 // global array for data to use in table
 var currentResults = [];
 
+// keyword that is currently being looked at
+var currentKeyword = "";
+
 $(document).ready(function () {
+    // search upon pressing enter
+    $("#sentence").keyup(function(event) {
+        if (event.keyCode === 13) {
+            $("#define").click();
+        }
+    });
+
     // click event handlers for dynamic elements (https://stackoverflow.com/a/9331127)
     $("#keywordLinks").on("click", "a.list-group-item", function() {
-        // console.log($(this).text());
+        currentKeyword = $(this).text();
 
         for (var i = 0; i < currentResults.length; i++) {
-            var r = currentResults[i]; // easier to type
+            var r = currentResults[i]; // r = result, easier to type
 
-            if (r.keyword == $(this).text()) {
-                console.log(r.keyword);
-
-                // populate table with results here!
-                // r.data[0].japanese[0].word
-                // r.data[0].japanese[1].reading
-                // r.data[0].senses[0].english_definitions[#]
-
+            if (r.keyword === currentKeyword) {
+                // populate table with results
                 $("#results").find("tr:gt(0)").remove(); // clear rows w/o clearing headings
 
                 // e = entry
                 for (var e of r.data) {
-                    // this'll cut out alternate spellings/readings, so be careful
-                    var word = e.japanese[0].word !== undefined ? e.japanese[0].word : "";
                     var reading = e.japanese[0].reading !== undefined ? e.japanese[0].reading : "";
+                    var word = e.japanese[0].word !== undefined ? e.japanese[0].word : reading;
 
                     // build english definition
                     var english = "";
                     
+                    // only get most common english definitions
+                    // words like 出る have >100 definitions and it would be inconvenient to show them all
                     for (var d of e.senses[0].english_definitions) {
                         english += (d + ", ")
                     }
 
                     english = english.slice(0, -2); // cut off trailing space + comma
 
-                    console.log(word + " | " + reading + " | " + english);
-
-                    $("#results").append("<tr><td>" + word + "</td><td>" + reading + "</td><td>" + english + "</td></tr>");
+                    $("#results tbody").append("<tr title='" + word + "'><td>" + word + "</td><td>" + reading + "</td><td>" + english + "</td></tr>");
                 }
             }
         }
     });
+
+    // detailed view of entry through modal
+    $("#results tbody").on("click", "tr", function() {
+        var currentWord = $(this).attr("title");
+
+        // set data for modal
+        $("#modalTitle").text(currentWord);
+
+        var currentRow = $(this).closest("tr").index(); // index of row
+
+        for (var i = 0; i < currentResults.length; i++) {
+            if (currentResults[i].keyword === currentKeyword) {
+                var modalEntry = currentResults[i].data[currentRow];
+
+                // show/hide common badge
+                $("#modalCommon").prop("hidden", !modalEntry.is_common); 
+
+                // words + readings
+                $("#modalWords").empty();
+                $("#modalReadings").empty();
+
+                for (var j of modalEntry.japanese) {
+                    // if there is no reading, word is guaranteed
+                    if (j.reading !== undefined) {
+                        $("#modalReadings").append(j.reading + "<br>");
+                    } else {
+                        $("#modalReadings").append(j.word + "<br>");
+                    }
+
+                    // if there is no word, reading is guaranteed
+                    if (j.word !== undefined) {
+                        $("#modalWords").append(j.word + "<br>");
+                    } else {
+                        $("#modalWords").append(j.reading + "<br>");
+                    }
+                }
+
+                // english
+                var english = "";
+                    
+                // only most common definitions
+                for (var d of modalEntry.senses[0].english_definitions) {
+                    english += (d + ", ")
+                }
+
+                english = english.slice(0, -2); // cut off trailing space + comma
+
+                $("#modalEnglish").text(english);
+
+                // if there are definitions not shown, provide link to view them on Jisho
+                if (modalEntry.senses.length > 1) {
+                    $("#modalEnglish").append("<br><a href='https://jisho.org/search/" + currentWord + "'>See more definitions on Jisho.org");
+                }
+
+                // parts of speech
+                var partsOfSpeech = "";
+
+                if (modalEntry.senses[0].parts_of_speech.length > 0) {
+                    for (var p of modalEntry.senses[0].parts_of_speech) {
+                        partsOfSpeech += (p + ", ");
+                    }
+
+                    partsOfSpeech = partsOfSpeech.slice(0, -2); // cut off trailing space + comma
+                }
+
+                $("#modalPartsOfSpeech").text(partsOfSpeech);
+
+                // info
+                var info = "";
+
+                if (modalEntry.senses[0].info.length > 0) {
+                    for (var f of modalEntry.senses[0].info) {
+                        info += (f, ", ");
+                    }
+
+                    info = info.slice(0, -2); // cut off trailing space + comma
+                }
+
+                $("#modalInfo").text(info);
+
+                // links
+                var links = "";
+
+                if (modalEntry.senses[0].links.length > 0) {
+                    for (var l of modalEntry.senses[0].links) {
+                        links += "<a href='" + l.url + "'>" + l.text + "</a><br>";
+                    }
+                }
+
+                $("#modalLinks").html(links);
+
+                // see also
+                var seeAlso = "";
+
+                if (modalEntry.senses[0].see_also.length > 0) {
+                    for (var sa of modalEntry.senses[0].see_also) {
+                        seeAlso += "<a href='https://jisho.org/search/" + sa + "'>" + sa + "</a><br>";
+                    }
+                }
+
+                $("#modalSeeAlso").html(seeAlso);
+
+                // tags
+                var tags = "";
+
+                if (modalEntry.senses[0].tags.length > 0) {
+                    for (var t of modalEntry.senses[0].tags) {
+                        tags += "<span class='badge badge-info'>" + t + "</span> ";
+                    }
+                }
+
+                $("#modalTags").html(tags);
+            }
+        }
+
+        // pop up the modal
+        $("#detailModal").modal("show");
+    });
 });
+
+var thankUser = function() {
+    if ($("#feedbackForm")[0].checkValidity()) {
+        alert("Thanks for the feedback! I'll try to get back to you within the next 48 hours.");
+        $("#feedbackModal").modal("hide");
+    }
+}
 
 var getDictEntries = function() {
     var words = [];
@@ -82,15 +210,14 @@ var getDictEntries = function() {
             }
         }
 
-        console.log(words);
-        // console.log(searchJisho(words));
         currentResults = searchJisho(words);
-        console.log(currentResults);
 
         // clear then set up links to words in list group
         $("#keywordLinks").empty();
         for (var w of words) {
-            $("#keywordLinks").append('<a href="#" class="list-group-item">' + w + '</a>');
+            if (w != "") {
+                $("#keywordLinks").append('<a href="#" class="list-group-item">' + w + '</a>');
+            }
         }
     }
 }
@@ -98,22 +225,23 @@ var getDictEntries = function() {
 // searches Jisho and returns an array containing results
 var searchJisho = function(query) {
     // cors.io is a proxy service for bypassing the "No 'Access-Control-Allow-Origin' header" error
-    // This is required to avoid making the user change their browser settings and also because GitHub pages
+    // this is required to avoid making the user change their browser settings and also because GitHub pages
     // seems to not support CORS (although https://stackoverflow.com/a/26417091 says otherwise?)
     var jishoAPI = "https://cors.io/?https://jisho.org/api/v1/search/words?"
     var results = [];
 
     for (var i = 0; i < query.length; i++) {
-        (function(index) {
-            $.getJSON(jishoAPI, {
-                keyword: query[i]
-            })
-            .done(function(data) {
-                console.log(data);
-                data.keyword = query[index];
-                results.push(data);
-            });
-        })(i);
+        if (query[i] != "") {
+            (function(index) {
+                $.getJSON(jishoAPI, {
+                    keyword: query[i]
+                })
+                .done(function(data) {
+                    data.keyword = query[index];
+                    results.push(data);
+                });
+            })(i);
+        }
     }
 
     return results;
